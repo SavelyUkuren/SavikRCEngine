aca8395c4dae5fe24adfc3d0dd7447621fc377ed
Savely <yoyos9.8split@icloud.com>
2024-07-29 14:08:07 +0300

    se_math

---
+++ src/engine/math/se_math.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Савелий Никулин on 29.07.2024.
+//
+
+#include "se_math.h"
+
+float se_math::to_radian(const float angle) {
+    return angle * (M_PI / 180);
+}
---
+++ src/engine/math/se_math.h
@@ -0,0 +1,19 @@
+//
+// Created by Савелий Никулин on 29.07.2024.
+//
+
+#ifndef SEMATH_H
+#define SEMATH_H
+
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
+class se_math {
+public:
+    static float to_radian(const float angle);
+};
+
+
+
+#endif //SEMATH_H
